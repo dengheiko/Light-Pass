ef1765e81cdc7d8ba0b28e6e40a4bb026f7ecac8
dengheiko <dengheiko@gmail.com>
2024-07-07 16:23:56 +0300

    Исправил управление игрока

--- Assets/Scripts/LightPassGame/PlayerController.cs
+++ Assets/Scripts/LightPassGame/PlayerController.cs
@@ -14,6 +14,7 @@ namespace LightPassGame
 
         private Vector2 _movementVector;
         private MovementDirection _movementDirection;
+        private MovementDirection _previousMovementDirection;
 
         private void OnMovement(InputAction.CallbackContext context)
         {
@@ -31,6 +32,7 @@ namespace LightPassGame
 
         private void InputUpdate()
         {
+            _previousMovementDirection = _movementDirection;
             if (Input.GetKey(KeyCode.W)) _movementDirection = MovementDirection.Up;
             else if (Input.GetKey(KeyCode.S)) _movementDirection = MovementDirection.Down;
             else if (Input.GetKey(KeyCode.D)) _movementDirection = MovementDirection.Right;
@@ -39,6 +41,16 @@ namespace LightPassGame
 
         private void PickTargetCell()
         {
+            if (_previousMovementDirection != _movementDirection && _moveDelta < .5f)
+            {
+                var tryCell = CurrentCell.NeighbourOnDirection(_movementDirection);
+                if (tryCell != null)
+                {
+                    _targetCell = tryCell;
+                    _moveDelta = 0;
+                }
+            }
+            
             if (_targetCell != null) return;
             _targetCell = CurrentCell.NeighbourOnDirection(_movementDirection);
             _moveDelta = 0;
@@ -48,6 +60,7 @@ namespace LightPassGame
         private void MoveUpdate()
         {
             if (_targetCell == null) return;
+            
             _moveDelta += speed * Time.deltaTime;
             
             if (_moveDelta >= 1)
@@ -55,6 +68,7 @@ namespace LightPassGame
                 transform.position = _targetCell.transform.position;
                 CurrentCell = _targetCell;
                 _targetCell = null;
+                _movementDirection = MovementDirection.Stay;
                 return;
             }
 
