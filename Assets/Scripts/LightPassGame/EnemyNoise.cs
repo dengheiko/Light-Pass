de3acb0fc01ec89b821adfe8264919afd8e4ce7d
dengheiko <dengheiko@gmail.com>
2024-07-19 00:42:09 +0300

    Голубой цвет игрока

---
+++ Assets/Scripts/LightPassGame/EnemyNoise.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace LightPassGame
+{
+    public class EnemyNoise : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float amplitude;
+        [SerializeField] private float yOffset;
+
+        private Transform _parentTransform;
+
+        private void Awake()
+        {
+            _parentTransform = transform.parent;
+        }
+
+        private void Update()
+        {
+            transform.position =
+                _parentTransform.position +
+                Vector3.right * (Mathf.PerlinNoise(Time.time * speed, 0) * amplitude) +
+                Vector3.up * (Mathf.PerlinNoise(0, (Time.time + yOffset) * speed) * amplitude);
+        }
+    }
+}
