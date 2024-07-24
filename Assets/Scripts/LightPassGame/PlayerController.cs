764322cf8f2950a9b9fb9c1f2e7b55602c89df97
dengheiko <dengheiko@gmail.com>
2024-07-25 00:18:17 +0300

    Настроил недерменированное управление с геймпадом

--- Assets/Scripts/LightPassGame/PlayerController.cs
+++ Assets/Scripts/LightPassGame/PlayerController.cs
@@ -6,10 +6,11 @@ namespace LightPassGame
     public class PlayerController : CellPosition
     {
         private const float InputThreshold = 0.1f;
+        private const float DistanceToCenter = 0.2f;
         
         [SerializeField] private float speed = 1;
+        [SerializeField] private float alignToPathSpeed = 5;
 
-        [SerializeField] private float distanceToDetectWall = .6f;
         
         private Vector3 _inputVector;
         private Cell _targetCell;
@@ -27,9 +28,12 @@ namespace LightPassGame
         {
             InputUpdate();
             UpdateTargetCell();
-            MoveX();
-            MoveY();
+            NormalMoveX();
+            NormalMoveY();
+            CornerMoveX();
+            CornerMoveY();
             UpdateCurrentCell();
+            AlignToPath();
         }
 
         private void InputUpdate()
@@ -68,14 +72,18 @@ namespace LightPassGame
             };
         }
 
-        private void MoveX()
+        private void NormalMoveX()
         {
             if (_movementType != MovementType.X)
                 return;
 
+            if (Mathf.Abs(transform.position.y - CurrentCell.transform.position.y) > DistanceToCenter)
+                return;
+            
+            
             transform.position += Vector3.right * (_inputVector.x * speed * Time.deltaTime);
             
-            if (CheckWall() &&
+            if (_targetCell == null &&
                 Mathf.Sign((CurrentCell.transform.position - transform.position).x * _inputVector.x) < 0)
             {
                 transform.position =
@@ -84,14 +92,17 @@ namespace LightPassGame
             }
         }
 
-        private void MoveY()
+        private void NormalMoveY()
         {
             if (_movementType != MovementType.Y)
+                return;
+            
+            if (Mathf.Abs(transform.position.x - CurrentCell.transform.position.x) > DistanceToCenter)
                 return;
 
             transform.position += Vector3.up * (_inputVector.y * speed * Time.deltaTime);
             
-            if (CheckWall() &&
+            if (_targetCell == null &&
                 Mathf.Sign((CurrentCell.transform.position - transform.position).y * _inputVector.y) < 0)
             {
                 transform.position =
@@ -100,19 +111,72 @@ namespace LightPassGame
             }
         }
 
+        private void CornerMoveX()
+        {
+            if (_movementType != MovementType.Y)
+                return;
+            
+            if (Mathf.Abs(transform.position.x - CurrentCell.transform.position.x) <= DistanceToCenter)
+                return;
+
+            if (_targetCell == null)
+                return;
+            
+            transform.position += 
+                Vector3.right * 
+                (Mathf.Sign(CurrentCell.transform.position.x - transform.position.x) * _inputVector.magnitude * speed * Time.deltaTime);
+        }
+
+        private void CornerMoveY()
+        {
+            if (_movementType != MovementType.X)
+                return;
+            
+            if (Mathf.Abs(transform.position.y - CurrentCell.transform.position.y) <= DistanceToCenter)
+                return;
+
+            if (_targetCell == null)
+                return;
+            
+            transform.position += 
+                Vector3.up * 
+                (Mathf.Sign(CurrentCell.transform.position.y - transform.position.y) * _inputVector.magnitude * speed * Time.deltaTime);
+        }
+
 
         private bool CheckWall() => _targetCell == null;
-        
+
 
         private void UpdateCurrentCell()
         {
             if (CurrentDelta().magnitude < .5f) return;
+            if (_targetCell == null) return;
             CurrentCell = _targetCell;
         }
 
         private Vector3 CurrentDelta()
         {
             return transform.position - CurrentCell.transform.position;
+        }
+
+        private void AlignToPath()
+        {
+            switch (_movementType)
+            {
+                case MovementType.X:
+                    transform.position = Vector3.Lerp(
+                        transform.position,
+                        Vector3.right * transform.position.x + Vector3.up * CurrentCell.transform.position.y,
+                        alignToPathSpeed * Time.deltaTime);
+                    break;
+                case MovementType.Y:
+                    transform.position = Vector3.Lerp(
+                        transform.position,
+                        Vector3.up * transform.position.y + Vector3.right * CurrentCell.transform.position.x,
+                        alignToPathSpeed * Time.deltaTime);
+                    break;
+                
+            }
         }
     }
 }
